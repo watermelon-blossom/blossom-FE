13d2bd912c9fc5ad08c9a04d46133450b0591e83
siwon <ksw3721@gmail.com>
2024-05-30 22:01:30 +0900

    [KAN-19] SlideModal 컴포넌트 구현
    - SlideModal 컴포넌트 구현
    - yarn으로 패키지 관리 툴 일원화
    - svg 사용 라이브러리로 react-native-svg 사용

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import SlideModal, { ActionSheetRefType } from "@/components/ui/SlideModal";
+import { useRef } from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function TestScreen() {
+  const actionSheetRef = useRef<ActionSheetRefType>(null);
+
+  const handlePress = () => {
+    actionSheetRef.current?.show();
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>test</Text>
+      <Pressable onPress={handlePress}>
+        <Text>open</Text>
+      </Pressable>
+
+      <SlideModal ref={actionSheetRef}>
+        <View style={{ alignItems: "center", gap: 20 }}>
+          <Text style={{ fontSize: 20, fontWeight: "600" }}>Next Level!</Text>
+          <Image
+            source={{
+              uri: "https://mblogthumb-phinf.pstatic.net/MjAyMjA1MjdfODAg/MDAxNjUzNjA1MjY1MDUw.pajYqhH0CIiJZUB9Y7yRmyOR-b7eCQM3-m1qNMLoE_0g.Kt9cgcFyDqhmmPJBxof_E42VhMC3oWhg615jv1eD_TQg.JPEG.vitaminnala/9D629C35%EF%BC%8DEA10%EF%BC%8D4040%EF%BC%8D919F%EF%BC%8D120C0E036B1A.jpeg?type=w800",
+            }}
+            style={{ width: "100%", height: 400 }}
+          />
+        </View>
+      </SlideModal>
     </View>
   );
 }
