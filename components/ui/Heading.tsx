aa556e5e68ad81d7b06ee1f510700fe70366c910
Hyunwoo <gus007dn@naver.com>
2024-06-10 17:56:19 +0900

    [KAN-38] Heading 컴포넌트 구현

--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -1,24 +1,19 @@
-// components/Heading.tsx
 import { font } from "@/constants/font";
 import { wScale } from "@/util/responsive.util";
 import React from "react";
 import { Text, StyleSheet, TextProps } from "react-native";
 
-interface HeadingProps extends TextProps {
-  level: 1 | 2 | 3 | 4 | 5 | 6;
+type HeadingProps = TextProps & {
   children: React.ReactNode;
-}
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+};
 
-export default function Heading({
-  level,
-  children,
-  style,
-  ...props
-}: HeadingProps) {
-  const textStyle = [styles[`h${level}`], style];
-
+export default function Heading({ children, level, ...props }: HeadingProps) {
   return (
-    <Text style={textStyle} {...props}>
+    <Text
+      style={[styles[`h${level}`], { fontFamily: font.family.BM }]}
+      {...props}
+    >
       {children}
     </Text>
   );
@@ -27,26 +22,20 @@ export default function Heading({
 const styles = StyleSheet.create({
   h1: {
     fontSize: wScale(34),
-    fontFamily: font.family.BM,
   },
   h2: {
     fontSize: wScale(28),
-    fontFamily: font.family.BM,
   },
   h3: {
     fontSize: wScale(24),
-    fontFamily: font.family.BM,
   },
   h4: {
     fontSize: wScale(18),
-    fontFamily: font.family.BM,
   },
   h5: {
     fontSize: wScale(16),
-    fontFamily: font.family.BM,
   },
   h6: {
     fontSize: wScale(14),
-    fontFamily: font.family.BM,
   },
 });
