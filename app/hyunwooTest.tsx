343622328c79ad35c5f57e83b3de044ac737f590
starz <gus007dn@naver.com>
2024-06-10 18:22:35 +0900

    [KAN-37] CText 컴포넌트 구현 (#12)
    * [KAN-37] CText 컴포넌트 구현
    
    * [KAN-37] CText 컴포넌트 구현

--- app/hyunwooTest.tsx
+++ app/hyunwooTest.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 
-import Heading from "@/components/ui/Heading";
+import CText from "@/components/ui/CText";
+import { colors } from "@/constants/colors";
 
 export default function TestScreen() {
   const handlePressButton = () => {
@@ -9,12 +10,25 @@ export default function TestScreen() {
 
   return (
     <View style={styles.screen}>
-      <Heading level={1}>H1: Hyunwoo Test</Heading>
-      <Heading level={2}>H2: Hyunwoo Test</Heading>
-      <Heading level={3}>H3: Hyunwoo Test</Heading>
-      <Heading level={4}>H4: Hyunwoo Test</Heading>
-      <Heading level={5}>H5: Hyunwoo Test</Heading>
-      <Heading level={6}>H6: Hyunwoo Test</Heading>
+      <CText size="xs" color={colors.theme.primary}>
+        Hyunwoo Test
+      </CText>
+      <CText size="sm" color={colors.theme.black}>
+        Hyunwoo Test
+      </CText>
+      <CText size="md" color={colors.theme.primary}>
+        안녕하세요. 저는 지금 워터멜론 팀과 함께 블라썸 소개팅 앱을 만들고
+        있습니다.
+      </CText>
+      <CText size="lg" color={colors.theme.black}>
+        Hyunwoo Test
+      </CText>
+      <CText size="xl" color={colors.theme.primary}>
+        Hyunwoo Test
+      </CText>
+      <CText size="2xl" color={colors.theme.black}>
+        Hyunwoo Test
+      </CText>
     </View>
   );
 }
