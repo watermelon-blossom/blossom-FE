051a1a4a4c55e6a1d21d3e6386cb8414921259f8
starz <gus007dn@naver.com>
2024-06-10 22:27:38 +0900

    [KAN-17] AvatarInput 컴포넌트 구현 (#10)
    * [KAN-17] AvatarInput 컴포넌트 구현
    
    * [KAN-17] AvatarInput 컴포넌트 구현
    
    * [KAN-17] AvatarInput 컴포넌트 구현
    
    * [KAN-17] AvatarInput 컴포넌트 구현

--- app/hyunwooTest.tsx
+++ app/hyunwooTest.tsx
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
 
+import AvatarInput from "@/components/ui/AvatarInput";
 import CText from "@/components/ui/CText";
-import { colors } from "@/constants/colors";
+import { theme } from "@/constants/colors";
 
 export default function TestScreen() {
   const handlePressButton = () => {
@@ -10,25 +11,12 @@ export default function TestScreen() {
 
   return (
     <View style={styles.screen}>
-      <CText size="xs" color={colors.theme.primary}>
-        Hyunwoo Test
+      <CText size="xl" color={theme.primary}>
+        Hyunwoo Test Screen
       </CText>
-      <CText size="sm" color={colors.theme.black}>
-        Hyunwoo Test
-      </CText>
-      <CText size="md" color={colors.theme.primary}>
-        안녕하세요. 저는 지금 워터멜론 팀과 함께 블라썸 소개팅 앱을 만들고
-        있습니다.
-      </CText>
-      <CText size="lg" color={colors.theme.black}>
-        Hyunwoo Test
-      </CText>
-      <CText size="xl" color={colors.theme.primary}>
-        Hyunwoo Test
-      </CText>
-      <CText size="2xl" color={colors.theme.black}>
-        Hyunwoo Test
-      </CText>
+      <View style={{ flexDirection: "row" }}>
+        <AvatarInput />
+      </View>
     </View>
   );
 }
--- assets/Icons/index.ts
+++ assets/Icons/index.ts
@@ -3,3 +3,5 @@ export { default as more } from "./more.svg";
 export { default as send } from "./send.svg";
 export { default as setting } from "./setting.svg";
 export { default as sort } from "./sort.svg";
+export { default as camera } from "./camera.svg";
+export { default as people } from "./people.svg";
