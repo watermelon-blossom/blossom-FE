44b58e1e67293a0a46000ffe41b2cf2d14f3254d
starz <gus007dn@naver.com>
2024-06-12 14:46:35 +0900

    [KAN-21] QuestionText 컴포넌트 구현 (#13)
    * [KAN-21] QuestionText 컴포넌트 구현
    
    * [KAN-21] QuestionText 컴포넌트 구현

--- app/hyunwooTest.tsx
+++ app/hyunwooTest.tsx
@@ -1,22 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
-import AvatarInput from "@/components/ui/AvatarInput";
-import CText from "@/components/ui/CText";
-import { theme } from "@/constants/colors";
+import QuestionText from "@/components/ui/QuestionText";
+import useCountStore from "@/store/useCountStore";
+import RadioButton from "@/components/ui/RadioButton";
+import { useState } from "react";
 
+const QUESTIONTEXT_LIST = [
+  {
+    QuestionNumber: 1,
+    Content: "선호하는 데이트 스타일은?",
+    S1: { label: "실내 데이트", value: "inside" },
+    S2: { label: "실외 데이트", value: "outside" },
+  },
+  {
+    QuestionNumber: 2,
+    Content: "내가 더 좋아하는 음식은?",
+    S1: { label: "한식", value: "korean" },
+    S2: { label: "양식", value: "european" },
+  },
+];
 export default function TestScreen() {
-  const handlePressButton = () => {
+  const [userInput, setUserInput] = useState({
+    q1: "",
+  });
+
+  const { count, setNumber } = useCountStore((state) => state);
+
+  const handlePressButton = (name: string, value: string) => {
     console.log("press");
+    setUserInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    setTimeout(() => {
+      setNumber(count ? 0 : 1);
+    }, 500);
   };
 
   return (
     <View style={styles.screen}>
-      <CText size="xl" color={theme.primary}>
-        Hyunwoo Test Screen
-      </CText>
-      <View style={{ flexDirection: "row" }}>
-        <AvatarInput />
-      </View>
+      <QuestionText
+        num={QUESTIONTEXT_LIST[count].QuestionNumber}
+        question={QUESTIONTEXT_LIST[count].Content}
+      />
+      <Image
+        source={require("@/assets/images/date.png")}
+        style={{ width: "100%", aspectRatio: 1.5 }}
+        resizeMode="contain"
+      ></Image>
+      <RadioButton
+        menus={[QUESTIONTEXT_LIST[count].S1, QUESTIONTEXT_LIST[count].S2]}
+        name="q1"
+        value={userInput.q1}
+        onSelectMenu={handlePressButton}
+      ></RadioButton>
     </View>
   );
 }
@@ -24,7 +61,7 @@ export default function TestScreen() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
     gap: 20,
     padding: 20,
