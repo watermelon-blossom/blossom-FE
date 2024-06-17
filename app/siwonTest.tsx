0b3857ae7b412ae61f4f7e340121b131b0909248
p.space <39205612+k-cool@users.noreply.github.com>
2024-06-17 21:26:30 +0900

    [KAN-23] QCarousel 컴포넌트 구현 (#15)
    - QCarousel 컴포넌트 구현
    - PaginationDots 컴포넌트 구현

--- app/siwonTest.tsx
+++ app/siwonTest.tsx
@@ -1,34 +1,53 @@
-import Header from "@/components/ui/Header";
-import IconButton from "@/components/ui/IconButton";
-import { router } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import Button from "@/components/ui/Button";
+import PaginationDots from "@/components/ui/PaginationDots";
+import React, { useRef, useState } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { theme } from "@/constants/colors";
+
+import QCarousel, { QCarouselDataProp } from "@/components/ui/QCarousel";
+import { ICarouselInstance } from "react-native-reanimated-carousel";
+
+const DATA: QCarouselDataProp = [
+  require("../assets/images/test1.png"),
+  require("../assets/images/test2.png"),
+  require("../assets/images/test3.png"),
+];
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function TestScreen() {
-  return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Header
-          left={
-            <IconButton
-              onPress={() => {
-                router.back();
-              }}
-            />
-          }
-          right={<IconButton iconName="setting" onPress={() => {}} />}
-          title="Discover"
-          subTitle="Chicago, IL"
-          // isTitleLeft
-        />
+  const carouselRef = useRef<ICarouselInstance>(null);
+  const [currentIdx, setCurrentIdx] = useState(0);
 
-        <View style={styles.screen}>
-          <Pressable onPress={() => {}}>
-            <Text>open</Text>
-          </Pressable>
-        </View>
-      </SafeAreaView>
-    </>
+  const handleNextSlide = () => {
+    if (!carouselRef.current) return;
+    carouselRef.current.next();
+  };
+
+  const handlePrevSlide = () => {
+    if (!carouselRef.current) return;
+    carouselRef.current.prev();
+  };
+
+  return (
+    <View style={styles.screen}>
+      <QCarousel
+        ref={carouselRef}
+        data={DATA}
+        width={SCREEN_WIDTH * 0.9}
+        height={SCREEN_HEIGHT * 0.6}
+        onChangeSlide={(idx) => {
+          setCurrentIdx(idx);
+        }}
+      />
+      <PaginationDots
+        totalItems={DATA.length}
+        currentIndex={currentIdx}
+        activeDotStyle={{ backgroundColor: "yellow" }}
+      />
+      <Button onPress={handlePrevSlide}>Prev</Button>
+      <Button onPress={handleNextSlide}>Next</Button>
+    </View>
   );
 }
 
@@ -38,6 +57,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     gap: 20,
-    backgroundColor: "yellowgreen",
+    padding: 20,
+    backgroundColor: theme.contrast,
   },
 });
---
+++ components/ui/QCarousel.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { View, StyleSheet, ImageSourcePropType, Image } from "react-native";
+import Animated, {
+  SharedValue,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import ReCarousel, {
+  ICarouselInstance,
+} from "react-native-reanimated-carousel";
+import { wScale } from "@/util/responsive.util";
+
+export type QCarouselDataProp = ImageSourcePropType[];
+
+type QCarouselProps = {
+  data: QCarouselDataProp;
+  width: number;
+  height: number;
+  enableSwipe?: boolean;
+  defaultIndex?: number;
+  onChangeSlide?: (index: number) => void;
+};
+
+type RenderItemParams = {
+  index: number;
+  item: ImageSourcePropType;
+};
+
+const QCarousel = React.forwardRef<ICarouselInstance, QCarouselProps>(
+  (
+    { data, width, height, enableSwipe = false, defaultIndex, onChangeSlide },
+    ref
+  ) => {
+    const pressAnimation = useSharedValue(0);
+
+    const animationStyle = (value: number) => {
+      "worklet";
+
+      const translateX = interpolate(value, [-1, 0, 1], [-width, 0, width]);
+
+      return {
+        transform: [{ translateX }],
+      };
+    };
+
+    return (
+      <View style={[{ width, height }]}>
+        <ReCarousel
+          ref={ref}
+          data={data}
+          width={width}
+          height={height}
+          defaultIndex={defaultIndex}
+          onScrollBegin={() => {
+            pressAnimation.value = withTiming(1, { duration: 300 });
+          }}
+          onScrollEnd={() => {
+            pressAnimation.value = withTiming(0, { duration: 300 });
+          }}
+          onSnapToItem={onChangeSlide}
+          scrollAnimationDuration={1000}
+          customAnimation={animationStyle}
+          overscrollEnabled={false}
+          snapEnabled={false}
+          enabled={enableSwipe}
+          renderItem={({ index, item }: RenderItemParams) => (
+            <CarouselItem
+              key={index}
+              source={item}
+              pressAnimation={pressAnimation}
+            />
+          )}
+        />
+      </View>
+    );
+  }
+);
+
+export default QCarousel;
+
+export type CarouselItemProps = {
+  pressAnimation: SharedValue<number>;
+  source: ImageSourcePropType;
+};
+
+const CarouselItem = ({ pressAnimation, source }: CarouselItemProps) => {
+  const animatedStyle = useAnimatedStyle(() => {
+    const scale = interpolate(pressAnimation.value, [0, 1], [1, 0.85]);
+
+    return {
+      transform: [{ scale }],
+    };
+  }, []);
+
+  return (
+    <Animated.View style={[itemStyles.container, animatedStyle]}>
+      <View style={itemStyles.imgWrapper}>
+        <Image
+          source={source}
+          resizeMode="cover"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </View>
+    </Animated.View>
+  );
+};
+
+const itemStyles = StyleSheet.create({
+  container: {
+    alignContent: "center",
+    justifyContent: "center",
+    gap: wScale(20),
+  },
+  imgWrapper: {
+    width: "100%",
+    height: "100%",
+    borderRadius: wScale(30),
+    overflow: "hidden",
+  },
+});
