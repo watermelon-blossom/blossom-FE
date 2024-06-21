f3d455afb89d67f7e6bc4e8a9a1a159ffd65404d
Hyunwoo <gus007dn@naver.com>
2024-06-21 14:38:24 +0900

    [KAN-48] 스와이프 기능 구현

--- app/hyunwooTest.tsx
+++ app/hyunwooTest.tsx
@@ -1,39 +1,54 @@
-import CustomCalendar from "@/components/ui/CustomCalendar";
-import SlideModal, { SlideModalRefType } from "@/components/ui/SlideModal";
-import { useRef, useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import moment, { Moment } from "moment";
-import CText from "@/components/ui/CText";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Card, { CARD_DATA } from "@/components/ui/Card";
 import { theme } from "@/constants/colors";
+import IconButton from "@/components/ui/IconButton";
+import SvgIcon from "@/components/ui/SvgIcon";
+import Icon from "@/assets/Icons/back.svg";
+import { useState } from "react";
+import { useSharedValue } from "react-native-reanimated";
 
+const cardData: CARD_DATA[] = [
+  {
+    item: { title: "test1", image: require("../assets/images/test1.png") },
+  },
+  {
+    item: { title: "test2", image: require("../assets/images/test2.png") },
+  },
+  {
+    item: { title: "test3", image: require("../assets/images/test3.png") },
+  },
+];
 export default function TestScreen() {
-  const [selectDay, setSelectDay] = useState(moment().format("yyyy-MM-DD"));
-  const slideModalRef = useRef<SlideModalRefType>(null);
-
-  const handlePressButton = () => {
-    console.log("press");
-    slideModalRef.current?.show();
-  };
-
-  const handleSelectDate = (date: Moment) => {
-    setSelectDay(date.format("yyyy-MM-DD"));
-  };
-
+  // const [newData, setNewData] = useState([...data, ...data])
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const animatedValue = useSharedValue(0);
+  const MAX = 3;
   return (
-    <View style={styles.screen}>
-      <Pressable onPress={handlePressButton}>
-        <Text>open</Text>
-      </Pressable>
-      <CText size="2xl">{selectDay}</CText>
-
-      <SlideModal ref={slideModalRef}>
-        <CustomCalendar initialDate={selectDay} onDaySelect={handleSelectDate}>
-          <CText color={theme.black} size="sm">
-            생년월일
-          </CText>
-        </CustomCalendar>
-      </SlideModal>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.screen}>
+        <View style={styles.cardContainer}>
+          {cardData.map((data, index) => {
+            if (index > currentIndex + MAX || index < currentIndex) {
+              return null;
+            }
+            return (
+              <Card
+                item={data.item}
+                index={index}
+                key={index}
+                dataLength={cardData.length}
+                maxVisibleItem={MAX}
+                currentIndex={currentIndex}
+                animatedValue={animatedValue}
+                setCurrentIndex={setCurrentIndex}
+              ></Card>
+            );
+          })}
+        </View>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
@@ -44,5 +59,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 20,
     padding: 20,
+    backgroundColor: theme.contrast,
+  },
+  cardContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
---
+++ components/ui/Card.tsx
@@ -0,0 +1,145 @@
+import { wScale } from "@/util/responsive.util";
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
+import { theme } from "@/constants/colors";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  SharedValue,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+export type CARD_DATA = {
+  item: {
+    title: string;
+    image: ImageSourcePropType;
+  };
+};
+
+type CardProps = {
+  item: { title: string; image: ImageSourcePropType };
+  index: number;
+  dataLength: number;
+  maxVisibleItem: number;
+  currentIndex: number;
+  animatedValue: SharedValue<number>;
+  setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export default function Card({
+  item,
+  index,
+  dataLength,
+  maxVisibleItem,
+  currentIndex,
+  animatedValue,
+  setCurrentIndex,
+}: CardProps) {
+  const { width } = useWindowDimensions();
+  const translateX = useSharedValue(0);
+  const direction = useSharedValue(0);
+  const pan = Gesture.Pan()
+    .onUpdate((e) => {
+      const isSwipeRight = e.translationX > 0;
+      direction.value = isSwipeRight ? 1 : -1;
+
+      if (currentIndex === index) {
+        translateX.value = e.translationX;
+        animatedValue.value = interpolate(
+          Math.abs(e.translationX),
+          [0, width],
+          [index, index + 1]
+        );
+      }
+    })
+    .onEnd((e) => {
+      if (currentIndex === index) {
+        if (Math.abs(e.translationX) > 150 || Math.abs(e.velocityX) > 1000) {
+          translateX.value = withTiming(width * direction.value, {}, () => {
+            runOnJS(setCurrentIndex)(currentIndex + 1);
+          });
+          animatedValue.value = withTiming(currentIndex + 1);
+        } else {
+          translateX.value = withTiming(0, { duration: 500 });
+          animatedValue.value = withTiming(currentIndex + 1);
+        }
+      }
+    });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const currentItem = index === currentIndex;
+    const rotateZ = interpolate(
+      Math.abs(translateX.value),
+      [0, width],
+      [0, 50]
+    );
+    const translateY = interpolate(
+      animatedValue.value,
+      [index - 1, index],
+      [-30, 0]
+    );
+    const scale = interpolate(
+      animatedValue.value,
+      [index - 1, index],
+      [0.9, 1]
+    );
+    const opacity = interpolate(
+      animatedValue.value + maxVisibleItem,
+      [index, index + 1],
+      [0, 1]
+    );
+
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          scale: currentItem ? 1 : scale,
+        },
+        {
+          translateY: currentItem ? 0 : translateY,
+        },
+        {
+          rotateZ: currentItem ? `${direction.value * rotateZ}deg` : `0deg`,
+        },
+      ],
+      opacity: index < maxVisibleItem + currentIndex ? 1 : opacity,
+    };
+  });
+
+  return (
+    <GestureDetector gesture={pan}>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            zIndex: dataLength - index,
+          },
+          animatedStyle,
+        ]}
+      >
+        <Image source={item.image} style={styles.image} />
+      </Animated.View>
+    </GestureDetector>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    width: wScale(295),
+    height: wScale(450),
+    borderRadius: wScale(20),
+    backgroundColor: theme.primary,
+  },
+  imageContainer: { width: wScale(295), height: wScale(450) },
+  image: { width: "100%", height: "100%", borderRadius: wScale(20) },
+});
